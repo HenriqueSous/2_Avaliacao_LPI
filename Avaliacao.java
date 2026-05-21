0e119f879b311db9c1c44e5afe531314b18ced0d
Henrique Sousa <henriquesousaoliveira281@gmail.com>
2026-05-21 00:14:40 +0000

    Resolução da segunda Avaliaçao de LPI

--- Avaliacao.java
+++ Avaliacao.java
@@ -1,7 +1,28 @@
-
-/* Se você está vendo esta mensagem é porque fez tudo certo até aqui. 
-    Boa prova! */
-
 void main() {
-    IO.println("Boa Prova!");
+    int totalPieces = 0;
+    int batchesAnalyzed = 0;
+
+    while (true) {
+        int quantityPieces = Integer.parseInt(IO.readln("Insira a quantidade de peças defeituosas: "));
+        if (quantityPieces < 0) {
+            if (batchesAnalyzed == 0) {
+                IO.println("A quantidade de peças no primeiro lote deve ser maior que zero");
+            } else {
+                break;
+            }
+        } else {
+            totalPieces += quantityPieces;
+            batchesAnalyzed++;
+        }
+    }
+
+    double average = totalPieces / batchesAnalyzed;
+
+    if (average <= 2) {
+        IO.println("Máquina Regulada (Excelente)");
+    } else if (average > 2.1 && average < 5) {
+        IO.println("Alerta! Necessita de Manutenção Preventiva");
+    } else if (average > 5) {
+        IO.println("Paragem Crítica: Máquina Danificada");
+    }
 }
